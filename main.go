0fba7e237278faba238f7bed85b7ae6cc4b7bb75
Nandan Sridhar <srinandans@google.com>
2019-07-21 12:06:18 -0700

    add version

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"os"
-
 	"./cmd"
+	"os"
 )
 
+var GitVersion string
+
 func main() {
+
 	rootCmd := cmd.GetRootCmd()
+	rootCmd.Version = "0.2, Git: " + GitVersion
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(-1)
