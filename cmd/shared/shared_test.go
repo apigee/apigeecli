81b2e84cbac7e3b1032fbcf6a9e23e845129222a
Nandan Sridhar <srinandans@google.com>
2019-07-26 16:58:55 -0700

    add shared tests

---
+++ cmd/shared/shared_test.go
@@ -0,0 +1,35 @@
+package shared
+
+import (
+	"testing"
+)
+
+func TestInit(t *testing.T) {
+	Init()
+	Info.Println("Printing Information")
+	Warning.Println("Printing Warning")
+	Error.Println("Printing Error")
+}
+
+func TestHttpGet(t *testing.T) {
+	Init()
+	err := HttpClient("https://httpbin.org/get")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestHttpPost(t *testing.T) {
+	payload := "test"
+	err := HttpClient("https://httpbin.org/post", payload)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestHttpDelete(t *testing.T) {
+	err := HttpClient("https://httpbin.org/post", "", "DELETE")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
