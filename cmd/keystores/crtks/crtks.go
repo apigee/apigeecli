7c29b38291008f6daeaaf506b06b4f8ba711f0ff
srinandan <nandanks@gmail.com>
2019-12-01 15:28:13 -0800

    support create key stores

--- cmd/keystores/crtks/crtks.go
+++ cmd/keystores/crtks/crtks.go
@@ -1,7 +1,11 @@
 package crtks
 
 import (
+	"net/url"
+	"path"
+
 	"github.com/spf13/cobra"
+	"github.com/srinandan/apigeecli/cmd/shared"
 )
 
 //Cmd to create key stores
@@ -10,6 +14,20 @@ var Cmd = &cobra.Command{
 	Short: "Create a Key Store",
 	Long:  "Create a Key Store",
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
-		return nil
+		u, _ := url.Parse(shared.BaseURL)
+		u.Path = path.Join(u.Path, shared.RootArgs.Org, "environments", shared.RootArgs.Env, "keystores")
+		payload := "{\"name\":\"" + name + "\"}"
+		_, err = shared.HttpClient(true, u.String(), payload)
+		return
 	},
+}
+
+var name string
+
+func init() {
+
+	Cmd.Flags().StringVarP(&name, "name", "n",
+		"", "Name of the key store")
+
+	_ = Cmd.MarkFlagRequired("name")
 }
