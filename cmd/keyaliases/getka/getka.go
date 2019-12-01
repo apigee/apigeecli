bf696f46a84effc64eb6c8fd63407f079e223956
srinandan <nandanks@gmail.com>
2019-12-01 15:27:04 -0800

    support get key alias details

--- cmd/keyaliases/getka/getka.go
+++ cmd/keyaliases/getka/getka.go
@@ -11,8 +11,8 @@ import (
 //Cmd to get key aliases
 var Cmd = &cobra.Command{
 	Use:   "get",
-	Short: "Get a Key Store",
-	Long:  "Get a Key Store",
+	Short: "Get a Key Alias",
+	Long:  "Get a Key Alias",
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
 		u, _ := url.Parse(shared.BaseURL)
 		u.Path = path.Join(u.Path, shared.RootArgs.Org, "environments", shared.RootArgs.Env,
