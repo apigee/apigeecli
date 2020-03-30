d9b68b77a060a5b57a4fae926d9e501a49410ca4
srinandan <nandanks@gmail.com>
2020-03-30 14:01:07 -0700

    add support for env props

--- client/env/env.go
+++ client/env/env.go
@@ -15,6 +15,8 @@
 package env
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/url"
 	"path"
 
@@ -39,4 +41,71 @@ func List() (respBody []byte, err error) {
 	u.Path = path.Join(u.Path, apiclient.GetApigeeOrg(), "environments")
 	respBody, err = apiclient.HttpClient(apiclient.GetPrintOutput(), u.String())
 	return respBody, err
+}
+
+//SetEnvProperty is used to set env properties
+func SetEnvProperty(name string, value string) (err error) {
+	//EnvProperty contains an individual org flag or property
+	type envProperty struct {
+		Name  string `json:"name,omitempty"`
+		Value string `json:"value,omitempty"`
+	}
+	//EnvProperties stores all the org feature flags and properties
+	type envProperties struct {
+		Property []envProperty `json:"property,omitempty"`
+	}
+
+	//Env structure
+	type environment struct {
+		Name           string        `json:"name,omitempty"`
+		Description    string        `json:"description,omitempty"`
+		CreatedAt      string        `json:"-,omitempty"`
+		LastModifiedAt string        `json:"-,omitempty"`
+		Properties     envProperties `json:"properties,omitempty"`
+	}
+
+	u, _ := url.Parse(apiclient.BaseURL)
+	u.Path = path.Join(u.Path, apiclient.GetApigeeOrg(), "environments", apiclient.GetApigeeEnv())
+	//get env details
+	envBody, err := apiclient.HttpClient(false, u.String())
+	if err != nil {
+		return err
+	}
+
+	env := environment{}
+	err = json.Unmarshal(envBody, &env)
+	if err != nil {
+		return err
+	}
+
+	//check if the property exists
+	found := false
+	for i, properties := range env.Properties.Property {
+		if properties.Name == name {
+			fmt.Println("Property found, enabling property")
+			env.Properties.Property[i].Value = value
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		//set the property
+		newProp := envProperty{}
+		newProp.Name = name
+		newProp.Value = value
+
+		env.Properties.Property = append(env.Properties.Property, newProp)
+	}
+
+	newEnvBody, err := json.Marshal(env)
+	if err != nil {
+		return err
+	}
+
+	u, _ = url.Parse(apiclient.BaseURL)
+	u.Path = path.Join(u.Path, apiclient.GetApigeeOrg(), "environments", apiclient.GetApigeeEnv())
+	_, err = apiclient.HttpClient(apiclient.GetPrintOutput(), u.String(), string(newEnvBody), "PUT")
+
+	return err
 }
