0f4084635101599f18f9071d17d60a6c94cee90a
srinandan <nandanks@gmail.com>
2020-07-11 22:38:49 -0700

    add ops

---
+++ client/operations/operations.go
@@ -0,0 +1,38 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package operations
+
+import (
+	"net/url"
+	"path"
+
+	"github.com/srinandan/apigeecli/apiclient"
+)
+
+//Get
+func Get(name string) (respBody []byte, err error) {
+	u, _ := url.Parse(apiclient.BaseURL)
+	u.Path = path.Join(u.Path, apiclient.GetApigeeOrg(), "operations", name)
+	respBody, err = apiclient.HttpClient(apiclient.GetPrintOutput(), u.String())
+	return respBody, err
+}
+
+//List
+func List() (respBody []byte, err error) {
+	u, _ := url.Parse(apiclient.BaseURL)
+	u.Path = path.Join(u.Path, apiclient.GetApigeeOrg(), "operations")
+	respBody, err = apiclient.HttpClient(apiclient.GetPrintOutput(), u.String())
+	return respBody, err
+}
---
+++ cmd/ops/getops.go
@@ -0,0 +1,46 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ops
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/srinandan/apigeecli/apiclient"
+	"github.com/srinandan/apigeecli/client/operations"
+)
+
+//Cmd to get ops details
+var GetCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get operation of an org",
+	Long:  "Get operation of an org",
+	Args: func(cmd *cobra.Command, args []string) (err error) {
+		apiclient.SetApigeeOrg(org)
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		_, err = operations.Get(name)
+		return
+	},
+}
+
+var name string
+
+func init() {
+
+	GetCmd.Flags().StringVarP(&name, "name", "n",
+		"", "operation name")
+
+	_ = GetCmd.MarkFlagRequired("name")
+}
