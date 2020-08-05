3bf2bdcbc51bbb730defad1533ecd339ac5128ba
srinandan <nandanks@gmail.com>
2020-08-04 21:51:04 -0700

    support for crt env

---
+++ cmd/env/crtenv.go
@@ -0,0 +1,44 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package env
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/srinandan/apigeecli/apiclient"
+	"github.com/srinandan/apigeecli/client/env"
+)
+
+//Cmd to get env details
+var CreateCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get properties of an environment",
+	Long:  "Get properties of an environment",
+	Args: func(cmd *cobra.Command, args []string) (err error) {
+		apiclient.SetApigeeEnv(environment)
+		return apiclient.SetApigeeOrg(org)
+	},
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		_, err = env.Create()
+		return
+	},
+}
+
+func init() {
+
+	CreateCmd.Flags().StringVarP(&environment, "env", "e",
+		"", "Apigee environment name")
+
+	_ = CreateCmd.MarkFlagRequired("env")
+}
