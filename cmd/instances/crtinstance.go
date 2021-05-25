345511d15cbf95060ca4003a956a12469a369590
srinandan <nandanks@gmail.com>
2021-05-25 09:57:20 -0700

    support trial instances

--- cmd/instances/crtinstance.go
+++ cmd/instances/crtinstance.go
@@ -21,6 +21,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/srinandan/apigeecli/apiclient"
 	"github.com/srinandan/apigeecli/client/instances"
+	"github.com/srinandan/apigeecli/client/orgs"
 )
 
 //Cmd to create a new instance
@@ -32,17 +33,34 @@ var CreateCmd = &cobra.Command{
 		return apiclient.SetApigeeOrg(org)
 	},
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
-		re := regexp.MustCompile(`projects\/([a-zA-Z0-9_-]+)\/locations\/([a-zA-Z0-9_-]+)\/keyRings\/([a-zA-Z0-9_-]+)\/cryptoKeys\/([a-zA-Z0-9_-]+)`)
-		ok := re.Match([]byte(diskEncryptionKeyName))
-		if !ok {
-			return fmt.Errorf("custom role must be of the format projects/{project-id}/locations/{location}/keyRings/{test}/cryptoKeys/{cryptoKey}")
+
+		if billingType, err = orgs.GetOrgField("billingType"); err != nil {
+			return err
 		}
-		_, err = instances.Create(name, location, diskEncryptionKeyName, cidrRange)
+
+		if billingType != "EVALUATION" {
+			re := regexp.MustCompile(`projects\/([a-zA-Z0-9_-]+)\/locations\/([a-zA-Z0-9_-]+)\/keyRings\/([a-zA-Z0-9_-]+)\/cryptoKeys\/([a-zA-Z0-9_-]+)`)
+			ok := re.Match([]byte(diskEncryptionKeyName))
+			if !ok {
+				return fmt.Errorf("custom role must be of the format projects/{project-id}/locations/{location}/keyRings/{test}/cryptoKeys/{cryptoKey}")
+			}
+
+			if !isRangeValid(cidrRange) {
+				return fmt.Errorf("Valid ranges are SLASH_16,SLASH_17,SLASH_18,SLASH_19 or SLASH_20")
+			}
+		}
+
+		if billingType == "EVALUATION" {
+			_, err = instances.Create(name, location, "", "SLASH_23")
+		} else {
+			_, err = instances.Create(name, location, diskEncryptionKeyName, cidrRange)
+		}
 		return
 	},
 }
 
-var diskEncryptionKeyName, cidrRange string
+var diskEncryptionKeyName, cidrRange, billingType string
+var allowedRanges = []string{"SLASH_16", "SLASH_17", "SLASH_18", "SLASH_19", "SLASH_20"}
 
 func init() {
 
@@ -53,9 +71,17 @@ func init() {
 	CreateCmd.Flags().StringVarP(&diskEncryptionKeyName, "diskenc", "d",
 		"", "CloudKMS key name")
 	CreateCmd.Flags().StringVarP(&cidrRange, "cidr", "r",
-		"", "Peering CIDR Range; default is SLASH_16, other supported values SLASH_20")
+		"", "Peering CIDR Range; default is SLASH_16, other supported values SLASH_20, SLASH_23 for eval")
 
 	_ = CreateCmd.MarkFlagRequired("name")
 	_ = CreateCmd.MarkFlagRequired("location")
-	_ = CreateCmd.MarkFlagRequired("diskenc")
+}
+
+func isRangeValid(cidrRange string) bool {
+	for _, allowedRange := range allowedRanges {
+		if allowedRange == cidrRange {
+			return true
+		}
+	}
+	return false
 }
