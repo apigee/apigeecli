88b6e60fe0a8bf68dc9fc753ce43363532647be7
srinandan <13950006+srinandan@users.noreply.github.com>
2022-10-29 12:12:26 -0700

    import via folder sharedflows #104

--- cmd/sharedflows/crtsf.go
+++ cmd/sharedflows/crtsf.go
@@ -15,34 +15,70 @@
 package sharedflows
 
 import (
+	"fmt"
+	"os"
+	"path"
+
 	"github.com/apigee/apigeecli/apiclient"
+	"github.com/apigee/apigeecli/bundlegen/proxybundle"
 	"github.com/apigee/apigeecli/client/sharedflows"
 	"github.com/spf13/cobra"
 )
 
-//Cmd to create shared flow
+// Cmd to create shared flow
 var CreateCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Creates a sharedflow in an Apigee Org",
 	Long:  "Creates a sharedflow in an Apigee Org",
 	Args: func(cmd *cobra.Command, args []string) (err error) {
 		apiclient.SetApigeeEnv(env)
+		if sfZip != "" && sfFolder != "" {
+			return fmt.Errorf("sharedflow bundle (zip) and folder to a sharedflow cannot be combined")
+		}
+		if sfZip == "" && sfFolder == "" {
+			return fmt.Errorf("either sharedflow bundle (zip) or folder must be specified, not both")
+		}
+		if sfFolder != "" {
+			if _, err := os.Stat(sfFolder); os.IsNotExist(err) {
+				return err
+			}
+		}
 		return apiclient.SetApigeeOrg(org)
 	},
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
-		_, err = sharedflows.Create(name, proxy)
+		if sfZip != "" {
+			_, err = sharedflows.Create(name, sfZip)
+		} else if sfFolder != "" {
+			tmpDir, err := os.MkdirTemp("", "sf")
+			if err != nil {
+				return err
+			}
+			defer os.RemoveAll(tmpDir)
+
+			sfBundlePath := path.Join(tmpDir, name+".zip")
+
+			if err = proxybundle.GenerateArchiveBundle(sfFolder, sfBundlePath); err != nil {
+				return err
+			}
+			if _, err = sharedflows.Create(name, sfBundlePath); err != nil {
+				return err
+			}
+			return os.Remove(sfBundlePath)
+		}
 		return
 	},
 }
 
-var proxy string
+var sfZip, sfFolder string
 
 func init() {
 
 	CreateCmd.Flags().StringVarP(&name, "name", "n",
 		"", "Sharedflow name")
-	CreateCmd.Flags().StringVarP(&proxy, "proxy", "p",
-		"", "Sharedflow bundle path")
+	CreateCmd.Flags().StringVarP(&sfZip, "sf-zip", "p",
+		"", "Path to the Sharedflow bundle/zip file")
+	CreateCmd.Flags().StringVarP(&sfFolder, "sf-zip-folder", "f",
+		"", "Path to the Sharedflow Bundle; ex: ./test/sharedflowbundle")
 
 	_ = CreateCmd.MarkFlagRequired("name")
 }
